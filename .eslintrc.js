be1beac2635b0ae1f11f95fa1ac4a6507cfee79c
Rhys <rbrooker105@gmail.com>
2018-08-31 16:13:23 +0800

    add axios, styled-components and github pages, update manifest, title and favicon

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+      "browser": true,
+    },
+    "rules": {
+      "implicit-arrow-linebreak": 0,
+      "import/named": 0,
+      "react/forbid-prop-types": 0,
+      "max-len": [1, 120, 2, {ignoreComments: true}],
+      "quote-props": [1, "consistent-as-needed"],
+      "no-cond-assign": [2, "except-parens"],
+      "radix": 0,
+      "space-infix-ops": 0,
+      "no-unused-vars": [1, {"vars": "local", "args": "none"}],
+      "default-case": 0,
+      "no-else-return": 0,
+      "no-param-reassign": 0,
+      "quotes": 0,
+      "react/jsx-filename-extension": 0
+    }
+  };
--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { injectGlobal } from 'styled-components';
+import styled from 'styled-components';
+import globalCCS from './UI/globalCSS';
+
+injectGlobal`${globalCCS}`;
+
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background: #d3cce3;
+  background: linear-gradient(to bottom, #d3cce3, #e9e4f0);
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Wrapper>
     );
   }
 }
