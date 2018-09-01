0a6695c07d7570b9987313b9118f56a4b7413cad
Rhys <rbrooker105@gmail.com>
2018-09-01 20:18:50 +0800

    throttle and debounce axios requests

--- src/containers/SearchInput/SearchInput.js
+++ src/containers/SearchInput/SearchInput.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Autocomplete from 'react-autocomplete';
+import { throttle, debounce } from 'throttle-debounce';
 import { inputStyling } from '../../UI/globalCSS';
 import axios from 'axios';
 
 class SearchInput extends Component {
   constructor(props) {
     super(props);
+    this.autocompleteSearchDebounced = debounce(1000, this.retrieveGitHubData);
+    this.autocompleteSearchThrottled = throttle(500, this.retrieveGitHubData);
     this.state = {
       searchTerm: '',
       autocompleteData: [
@@ -17,32 +20,27 @@ class SearchInput extends Component {
     };
   }
 
-  retrieveGitHubData(searchText='stuff'){
-    const query = new URLSearchParams(searchText)
-    const url = `https://api.github.com/search/repositories?q=${query}page=1&per_page=10`;
+  retrieveGitHubData(searchText) {
+    const url = `https://api.github.com/search/repositories?q=${searchText}+in%3Aname&per_page=10`;
     axios.get(url)
       .then(response => {
+        console.log(response)
         const results = Object.keys(response.data.items).map(key => {
           const repo = response.data.items[key]
           return {
             id: repo.releases_url,
-            name: repo.name
+            name: repo.name,
+            url: repo.html_url,
           };
         })
         this.setState({ autocompleteData: results });
         })
       .catch(error => {
+        console.log(error);
         this.setState(
           {
             autocompleteData: [
-              {
-                id: 1,
-                name: 'Something went wrong with the request'
-              },
-              {
-                id: 2,
-                name: error
-              },
+              {id: 1, name: ''},
             ],
           }
         );
@@ -52,15 +50,20 @@ class SearchInput extends Component {
   renderItem(item, isHighlighted){
     return (
         <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-            {item.name}
+          {item.name}
         </div>   
     ); 
   }
 
   inputChangedHandler = (event) => {
-    const newSearchTerm = event.target.value;
-    this.retrieveGitHubData(newSearchTerm)
-    this.setState({ searchTerm: newSearchTerm });
+    this.setState({ searchTerm: event.target.value }, () => {
+      const { searchTerm } = this.state;
+      if (searchTerm.length < 5) {
+        this.autocompleteSearchThrottled(searchTerm);
+      } else {
+        this.autocompleteSearchDebounced(searchTerm);
+      }
+    });
   }
 
   onSelect = (val) => {
